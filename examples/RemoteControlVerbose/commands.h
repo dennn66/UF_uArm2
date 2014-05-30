50d098220bb255d4d3d7ebea8e42c8b47f03af70
dennn66 <dennn6696@gmail.com>
2014-05-30 15:02:40 +0600

    Verbose interface to communicate with uArm from Terminal There is several one symbol commands: setPosition   usage: h {stretchTemp}:{heightTemp}:{rotationTemp}:{handRotTemp}:{CATCH}   example: h 100:100:25:-20:0 Test   usage: j   example: j ManualCalibration   usage: i {initPosL}:{initPosR}   example: h 25 35

---
+++ examples/RemoteControlVerbose/commands.h
@@ -0,0 +1,23 @@
+/* Define single-letter commands that will be sent by the PC over the
+   serial link.
+*/
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#define ANALOG_READ    'a'
+#define GET_BAUDRATE   'b'
+#define PIN_MODE       'c'
+#define DIGITAL_READ   'd'
+#define SERVO_WRITE    's'
+#define SERVO_READ     't'
+#define UPDATE_PID     'u'
+#define DIGITAL_WRITE  'w'
+#define ANALOG_WRITE   'x'
+#define ARM_POSITION   'h'
+#define ARM_CALIBRATION 'i'
+#define ARM_ALERT      'f'
+#define ARM_TEST       'j'
+#endif
+
+
